a92b0c99ae3d7ccfc9747fe8e64a7a734d336e20
lawson1000 <lawsonolanrewaju@yahoo.com>
2023-02-22 02:35:11 +0100

    completed about section

---
+++ main.js
@@ -0,0 +1,13 @@
+const tabLinks = document.getElementsByClassName("tab-links");
+const tabContents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tabLink of tabLinks){
+        tabLink.classList.remove('active-link');
+    }
+    for(tabContent of tabContents){
+        tabContent.classList.remove('active-tab');
+    }
+    event.currentTarget.classList.add("active-link")
+    document.getElementById(tabname).classList.add("active-tab");
+}
