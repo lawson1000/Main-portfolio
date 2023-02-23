3395d2adb82ebbbd6ba51cf426f0ad7544ccc772
lawson1000 <lawsonolanrewaju@yahoo.com>
2023-02-23 17:06:02 +0100

    created contact and copyright section

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const tabLinks = document.getElementsByClassName("tab-links");
 const tabContents = document.getElementsByClassName("tab-contents");
+const sideMenu = document.getElementById("sideMenu");
 
 function opentab(tabname){
     for(tabLink of tabLinks){
@@ -10,4 +11,11 @@ function opentab(tabname){
     }
     event.currentTarget.classList.add("active-link")
     document.getElementById(tabname).classList.add("active-tab");
+}
+
+function openMenu(){
+    sideMenu.style.right= "0";
+}
+function closeMenu(){
+    sideMenu.style.right= "-200px";
 }
